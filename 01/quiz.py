cc85d5aaae2d238b8b4bfbf66139f6c54930a42e
Wolodmr <145836117+Wolodmr@users.noreply.github.com>
2023-10-09 17:58:48 +0500

    Update quiz.py

--- 01/quiz.py
+++ 01/quiz.py
@@ -1,10 +1,30 @@
+questions = ['Who is an foundator of Python?', 'How is represnted boolean data type in Python?', 'How much is 36+34?']
+answers = ['Guido van Rossum', 'bool', '70']
+q = zip(questions, answers)
+
+quizz = [] 
+for item in q:
+    quizz.append({'question': item[0], 'answer': item[1]})
 
 
-questions = ['Who founded python?', 'What is the shortened version of "boolean" in Python?', 'what is 23+71?',
-             'when was Python created?', 'To assign a variable, do we use = or ==?']
-
-answers = ['Guido van Rossum', 'bool', '94', '1991', '=']
-
-# Write a program that loops through the questions and adds 1 to the score if the player answers correctly
-# and 0 if they answer incorrectly
-# Print the score with a message of your choice at the end. You may also print a different message based on how well the user did.
+def quiz_game(qa):
+    score = 0
+    for q in qa:
+        print(q['question'])
+        answer = input('What\'s your answer? \n')
+        print()
+        if answer == q['answer']:
+            score +=1
+            print('Well done!')
+            print()
+        else:
+            print('No. Your answer is wrong, sorry! ')
+            print()
+    if score == len(questions):
+        print(f'Congrats! You\'ve win. Your score is {score}')
+        print()
+    else:
+        print(f'Your score is {score}')
+    print('Thanx for taking part!')
+    print()
+quiz_game(quizz)
